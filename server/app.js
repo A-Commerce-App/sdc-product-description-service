facaa5056fdfb3dd8b63dc0bdd5258187a5da7c7
John <j0hn4ll3nc4mpb3ll@gmail.com>
2020-12-03 03:25:53 -0500

    adds endpoints for crud api

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
-const Product = require('../server/database').Product;
+const Product = require('../database/mongo').Product;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,6 +18,34 @@ app.get('/api/products/:id', (req, res) => {
     }
   })
 });
+
+app.put('/api/products/edit/:id', (req, res) => {
+  //update_method(req.params.id)
+  .then(result => res.status(200).send(result))
+  .catch(err => {
+    console.log(err);
+    res.status(400).send(err);
+  })
+})
+
+app.post('/api/products/delete/:id', (req, res) => {
+  //delete_method(req.params.id)
+  .then(result => res.status(200).send(result))
+  .catch(err => {
+    console.log(err);
+    res.status(400).send(err);
+  })
+})
+
+app.post('/api/products/add/:id', (req, res) => {
+  //insert_method(req.params.id)
+  .then(result => res.status(200).send(result))
+  .catch(err => {
+    console.log(err);
+    res.status(400).send(err);
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
